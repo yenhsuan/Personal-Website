1a959a36de17d8b32b191cf76c334b5bfaa3d017
yenhsuan <yenhsuan.terry@gmail.com>
2017-12-29 17:44:15 +0800

    create: my personal website

---
+++ js/main.js
@@ -0,0 +1,75 @@
+$(document).ready(() => {
+    let scrollTop = 0;
+    $(window).scroll(() => {
+        scrollTop = $(window).scrollTop();
+        
+        if (scrollTop >= 20) {
+            $('.navbar').addClass('navbar-small');
+            $('.navbar-icon').addClass('navbar-icon-sm');
+            $('.navbar-logo').addClass('navbar-logo-sm');                  
+        } else if (scrollTop < 20) {
+            $('.navbar').removeClass('navbar-small');
+            $('.navbar-icon').removeClass('navbar-icon-sm'); 
+            $('.navbar-logo').removeClass('navbar-logo-sm');                 
+        }
+    });
+
+    $("a").on('click', (event) => {
+        if (event.currentTarget.hash !== "") {
+            if (event.currentTarget.hash === "#project") {
+                return;
+            } 
+
+            event.preventDefault();
+            $('html, body').animate({scrollTop: $(event.currentTarget.hash).offset().top}, 700, () => {
+                window.location.hash = event.currentTarget.hash;
+            });
+        } // End if
+    });
+
+    (function($, win) {
+        $.fn.inViewport = function(cb) {
+           return this.each(function(i,el){
+             function visPx(){
+               var H = $(this).height(),
+                   r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
+               return cb.call(el, Math.max(0, t>0? H-t : (b<H?b:H)));  
+             } visPx();
+             $(win).on("resize scroll", visPx);
+           });
+        };
+    }(jQuery, window));
+
+
+    $("#home").inViewport((px) => {
+        if (px) {
+            $("#li-home").addClass("active");
+            $("#li-about").removeClass("active");
+            $("#li-project").removeClass("active");
+        }
+    });
+
+    $("#about").inViewport((px) => {
+        if (px) {
+            $("#li-about").addClass("active");
+            $("#li-home").removeClass("active");
+            $("#li-project").removeClass("active");
+        }
+    });
+
+    $("#project-container").inViewport((px) => {
+        if (px) {
+            $("#li-project").addClass("active");
+            $("#li-home").removeClass("active");
+            $("#li-about").removeClass("active");
+        }
+    });
+});
+
+function openNav() {
+    $(".overlay").addClass('overlay-show');
+}
+
+function closeNav() {
+    $(".overlay").removeClass('overlay-show');
+}
