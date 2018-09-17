c22add4f4b645369944a45400585b9a1e7da3265
yenhsuan <yenhsuan.terry@gmail.com>
2018-09-17 03:03:30 -0700

    webpack environment

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+import $ from 'jquery'
+import jQuery from 'jquery'
+import '../css/normalize.css'
+import '../css/skeleton.css'
+import '../sass/main.scss'
+
 $(document).ready(() => {
     $('<img/>').attr('src', '../img/bg-home.jpg').on('load', function() {
         $(this).remove();
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const webpack = require('webpack')
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: [
+    path.join(__dirname, './js/main.js'),
+  ],
+  output: {
+    filename: 'bundle.[hash].js',
+    path: path.join(__dirname, '/public'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions:{
+          output: {
+            comments: false, // remove comments
+          }
+        }
+      })
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        include: path.join(__dirname, './template'),
+        use: [ {
+          loader: 'html-loader',
+          options: {
+            interpolate: true
+          }
+        }]
+      },
+      {
+        test: /\.(js)$/,
+        loader: 'babel-loader',
+        exclude: [
+          path.join(__dirname, './node_modules'),
+        ],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+        loader: 'file-loader?name=img/[name].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: path.join(__dirname, './template/index.html'),
+      favicon: path.join(__dirname, './favicon.ico'),
+      minify: {
+        collapseWhitespace: true,
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.[hash].css",
+    })
+  ],
+  devServer: {
+    host: '0.0.0.0',
+    port: '3000',
+    contentBase: path.join(__dirname, './public'),
+    overlay: {
+      errors: true,
+    },
+    publicPath: '/',
+    historyApiFallback: true,
+  },
+}
